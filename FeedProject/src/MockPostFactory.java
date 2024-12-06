6dd10920b25a0195140b9b55a62b0930814bd64b
Jim Stewart <jim.stewart@thelangschool.org>
2024-12-06 12:45:37 -0500

    moved data to text files

--- FeedProject/src/MockPostFactory.java
+++ FeedProject/src/MockPostFactory.java
@@ -1,33 +1,65 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MockPostFactory {
 
-    private String[] keywords;
-    private String[] titles = {
-        "5 Tips for {action} to Boost {benefit}!",
-        "The Ultimate Guide to {topic}",
-        "How to {goal} in {timeframe}",
-        "Top 10 {items} for {outcome}",
-        "Why {concept} is Important for {benefit}"
-    };
-    private String[] contentStrings;
-    private HashMap<String, String[]> categories;
+    private ArrayList<String> keywords;
+    private ArrayList<String> titles;
+    private ArrayList<String> contentStrings;
+    private HashMap<String, ArrayList<String>> categories;
+    private ArrayList<String[]> replace_text;
+    private ArrayList<String[]> replace_title;
 
     public MockPostFactory(){
-        
+        titles = readLines("titles.txt");
+        contentStrings = readLines("content.txt");
+        categories = readCategories();
+        keywords = new ArrayList<String>(categories.keySet());
+        replace_text = readReplace("replace_text.txt");
+        replace_title = readReplace("replace_title.txt");
     }
 
-    public ArrayList<String> readFile(String fileName){
+    private ArrayList<String[]> readReplace(String fileName){
+        ArrayList<String[]> rtrn = new ArrayList<>();
+        ArrayList<String> lines = readLines(fileName);
+        for(String line:lines){
+            rtrn.add(line.split("-"));
+        }
+        return rtrn;
+    }
+
+    private HashMap<String, ArrayList<String>> readCategories(){
+        HashMap<String, ArrayList<String>> rtrn = new HashMap<>();
+        ArrayList<String> lines = readLines("categories.txt");
+        for(String line:lines){
+            String[] splitLine = line.split(":");
+            ArrayList<String> phraseList = new ArrayList<>();
+            for(String phrase:splitLine[1].split(",")){
+                phraseList.add(phrase);
+            }
+            rtrn.put(splitLine[0],phraseList);
+        }
+        return rtrn;
+    }
+
+    private ArrayList<String> readLines(String fileName){
         ArrayList<String> rtrn = new ArrayList<>();
         FileReader f;
         try{
             f = new FileReader(fileName);
             BufferedReader br = new BufferedReader(f);
+            String line;
+            while((line = br.readLine()) != null){
+                rtrn.add(line);
+            }
         } catch (FileNotFoundException e){
+            System.out.println(e);
+        } catch (IOException e){
             System.out.println(e);
         }
         return rtrn;
