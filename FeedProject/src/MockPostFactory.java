6402d0853dde4c0c782b669f0be2850e74666277
Jim Stewart <jim.stewart@thelangschool.org>
2024-12-06 15:32:45 -0500

    unimplemented makePost method

--- FeedProject/src/MockPostFactory.java
+++ FeedProject/src/MockPostFactory.java
@@ -2,8 +2,10 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 public class MockPostFactory {
 
@@ -13,14 +15,20 @@ public class MockPostFactory {
     private HashMap<String, ArrayList<String>> categories;
     private ArrayList<String[]> replace_text;
     private ArrayList<String[]> replace_title;
+    private Random random;
 
-    public MockPostFactory(){
+   public MockPostFactory(){
+        random = new Random();
         titles = readLines("titles.txt");
         contentStrings = readLines("content.txt");
         categories = readCategories();
         keywords = new ArrayList<String>(categories.keySet());
         replace_text = readReplace("replace_text.txt");
         replace_title = readReplace("replace_title.txt");
+    }
+
+    public Post makePost(int daysBeforeNow, int dayRange){
+        return null;
     }
 
     private ArrayList<String[]> readReplace(String fileName){
@@ -65,4 +73,18 @@ public class MockPostFactory {
     }
 
 
-}
+    private LocalDateTime getRandomDate(int daysBeforeNow, int dayRange) {
+
+        // Get the current date and time
+        LocalDateTime startTime = LocalDateTime.now().minusDays(daysBeforeNow);
+        
+        // Generate a random number of seconds from now to five days from now
+        long secondsInDayRange = dayRange * 24 * 60 * 60; // 5 days in seconds
+        long randomSeconds = random.nextLong(0, secondsInDayRange);
+
+        // Add the random seconds to the current date and time
+        return startTime.plusSeconds(randomSeconds);
+    }
+
+
+ }
