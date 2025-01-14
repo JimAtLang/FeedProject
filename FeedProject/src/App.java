934e0ea691b455b4238e9ece84ba5f49a7cd5481
August Cornfeld <august.cornfeld@thelangschool.net>
2025-01-14 14:42:42 -0500

    merged2

--- FeedProject/src/App.java
+++ FeedProject/src/App.java
@@ -1,14 +1,9 @@
 
-import java.time.LocalDateTime;
-import java.util.LinkedList;
 import java.util.ArrayList;
 
 public class App {
 
-    Tester t;
-
     public App() {
-        t = new Tester();
         ArrayList<Post> posts = new ArrayList<>();
         //Post p= new Post("a post","abcdef",0,LocalDateTime.now(), "Yaboy", "cars"); 
 
@@ -20,7 +15,5 @@ public class App {
     //delete after testing
 
     Feed customFeed = new Feed();
-
-    f.setPosts (posts);
 
 }
